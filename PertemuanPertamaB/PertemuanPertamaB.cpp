a129a60504c6ed34c8f170576ec89a52fba81702
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-02-26 15:44:42 +0700

    Add project files.

---
+++ PertemuanPertamaB/PertemuanPertamaB.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{// begin
+    // numeric nPanjang, nLebar, nLuas
+    int nPanjang, nLebar, nLuas;
+    // display 'Masukkan Panjang = '
+    cout << "Masukkan Panjang = ";
+    // accept nPanjang
+    cin >> nPanjang;
+    // dispaly 'Masukkan Lebar = '
+    cout << "Masukkan Lebar = ";
+    // accept nLebar
+    cin >> nLebar;
+    // compute nLuas = nPanjang * nLebar
+    nLuas = nPanjang * nLebar;
+    // display 'Luasnya adalah = ' + nLuas
+    cout << "Luasnya adalah =  " << nLuas << endl;
+
+}
